d4b14a9272dade79b17c8cc469ad3e0eb0486204
insta <insta@DESKTOP-IAVPL5C>
2020-12-28 20:04:16 +0900

    과제,공부 201228

---
+++ src/com/javaex/jdbc/BookInsert.java
@@ -0,0 +1,61 @@
+package com.javaex.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BookInsert {
+
+	public static void main(String[] args) {
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			//values(seq_book_id.nextval, '우리들의 일그러진 영웅','다림', '1998-02-22',1);
+			String query = " insert into book values( seq_book_id.nextval , ? , ? , ?, ? )";
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, "26년");
+			pstmt.setString(2, "재미주의");
+			pstmt.setString(3, "12/02/04");
+			pstmt.setString(4, "5");
+			
+			int count = pstmt.executeUpdate();
+			
+		    // 4.결과처리
+			
+			System.out.println(count + "건이 저장되었습니다.");
+			
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {      
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+
+	}
+
+}
---
+++ src/com/javaex/jdbc/BookUpdate.java
@@ -0,0 +1,80 @@
+package com.javaex.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BookUpdate {
+
+	public static void main(String[] args) {
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+				
+		    // 3. SQL문 준비 / 바인딩 / 실행
+		    /*
+		     update book 
+			 set title = '유럽 도시 기행',
+			       pubs = '생각의 길',
+			       pub_date = '19/07/09'
+			 where book_id = 4;
+		     */
+			
+			String query =  "";
+			
+			
+			
+			
+			query += " update book ";
+			query += " set title = ?, ";
+			query += " pubs = ?, ";
+			query += " pub_date = ? ";
+			query += " where book_id = ? ";
+			
+			System.out.println(query);
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, "유럽 도시 기행");
+			pstmt.setString(2, "생각의 길");
+			pstmt.setString(3, "19/07/09");
+			pstmt.setInt(4, 4);
+			
+			int count = pstmt.executeUpdate();
+			
+		    // 4.결과처리
+			
+			System.out.println(count + "건이 수정되었습니다.");
+
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {              
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+
+	}
+
+}
